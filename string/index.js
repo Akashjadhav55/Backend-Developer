e01e2b2605caf5739b69ccfa33b4256f09e41206
Akashjadhav55 <akashjadhav7040253178@gmail.com>
2024-06-25 18:38:39 +0530

    First Command

---
+++ string/index.js
@@ -0,0 +1,13 @@
+import express from 'express';
+let app = express()
+
+app.get("/shout", (req, res) => {
+    let name = req.query.date
+    console.log(name)
+    res.send(name.toUpperCase())
+})
+
+let PORT = 3000
+app.listen(PORT, () => {
+    console.log("server is running on https://" + PORT)
+})
